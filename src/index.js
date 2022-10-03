370ad746843afd83f63a3e30cb9bf647f233fda8
Rami B <ramibasheer9@gmail.com>
2022-10-03 16:41:07 +0200

    apply solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import './styles/main.scss'; // Don't delete :)
+
+import './styles/main.scss'; // Don't delete :)
 import smile from './scripts/smile.js'; // Feel free to delete :)
+import moment from 'moment';
 
-// All of your javascript should go here :)
+new CountDown("#countdown", { year: 2025, month: 2, day: 4, hour: 16 })
+// const newYear = new Date('January 1, 2023 00:00:00');
+
+// console.log(newYear);
+
+// const now = Date.now();
+// console.log(now);
+
+// const diff = newYear - now;
+// console.log(moment(diff).format());
 
 
-smile() // Feel free to delete :)
+
